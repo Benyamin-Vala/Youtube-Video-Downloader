6f037ea1f59755bfa561ab369eb481fba58bb6eb
Benyamin-Vala <102605148+Benyamin-Vala@users.noreply.github.com>
2022-04-11 18:09:55 +0300

    Delete New Text Document.bat

--- New Text Document.bat
+++
@@ -1,7 +0,0 @@
-echo "Youtube Downloader With Python. install python and library pytube3" >> README.md
-git init
-git add .
-git commit -m "Upload YoutubeDownloader on Github"
-git branch -M main
-git remote add origin https://github.com/Benyamin-Vala/Youtube-Video-Downloader.git
-git push -u origin main
